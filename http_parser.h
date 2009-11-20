bd291ab5d8ae2707d8ec9144d4c4d822cd334545
Ryan Dahl <ry@tinyclouds.org>
2009-11-20 15:51:36 +0100

    add license file

--- http_parser.h
+++ http_parser.h
@@ -1,4 +1,4 @@
-/* Copyright 2008, 2009 Ryan Dahl <ry@tinyclouds.org>
+/* Copyright 2009 Ryan Dahl <ry@tinyclouds.org>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
