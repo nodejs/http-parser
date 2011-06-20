16a7edd77a3b1e7357611cab8229a34d7bb444c2
Jon Kolb <jon@b0g.us>
2011-06-20 10:04:02 -0400

    Make IS_ALPHA use LOWER internally

--- http_parser.c
+++ http_parser.c
@@ -315,9 +315,9 @@ enum header_states
 #define LF                  '\n'
 #define LOWER(c)            (unsigned char)(c | 0x20)
 #define TOKEN(c)            (tokens[(unsigned char)c])
-#define IS_ALPHA(c)         ((c) >= 'a' && (c) <= 'z')
+#define IS_ALPHA(c)         (LOWER(c) >= 'a' && LOWER(c) <= 'z')
 #define IS_NUM(c)           ((c) >= '0' && (c) <= '9')
-#define IS_ALPHANUM(c)      (IS_ALPHA(LOWER(c)) || IS_NUM(c))
+#define IS_ALPHANUM(c)      (IS_ALPHA(c) || IS_NUM(c))
 
 #if HTTP_PARSER_STRICT
 #define IS_URL_CHAR(c)      (normal_url_char[(unsigned char) (c)])
@@ -670,7 +670,7 @@ size_t http_parser_execute (http_parser *parser,
 
         CALLBACK2(message_begin);
 
-        if (!IS_ALPHA(LOWER(ch))) {
+        if (!IS_ALPHA(ch)) {
           SET_ERRNO(HPE_INVALID_METHOD);
           goto error;
         }
@@ -767,13 +767,11 @@ size_t http_parser_execute (http_parser *parser,
           break;
         }
 
-        c = LOWER(ch);
-
         /* Proxied requests are followed by scheme of an absolute URI (alpha).
          * CONNECT is followed by a hostname, which begins with alphanum.
          * All other methods are followed by '/' or '*' (handled above).
          */
-        if (IS_ALPHA(c) || (parser->method == HTTP_CONNECT && IS_NUM(ch))) {
+        if (IS_ALPHA(ch) || (parser->method == HTTP_CONNECT && IS_NUM(ch))) {
           MARK(url);
           state = (parser->method == HTTP_CONNECT) ? s_req_host : s_req_schema;
           break;
@@ -785,9 +783,7 @@ size_t http_parser_execute (http_parser *parser,
 
       case s_req_schema:
       {
-        c = LOWER(ch);
-
-        if (IS_ALPHA(c)) break;
+        if (IS_ALPHA(ch)) break;
 
         if (ch == ':') {
           state = s_req_schema_slash;
