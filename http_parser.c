9c1f4f0b6c4c1510b2732012454d0699effb20d6
Ivo Raisr <ivosh@ivosh.net>
2012-01-27 15:49:27 +0100

    do not use C99 comments

--- http_parser.c
+++ http_parser.c
@@ -1850,7 +1850,7 @@ const char * http_method_str (enum http_method m)
 void
 http_parser_init (http_parser *parser, enum http_parser_type t)
 {
-  void *data = parser->data; // preserve application data
+  void *data = parser->data; /* preserve application data */
   memset(parser, 0, sizeof(*parser));
   parser->data = data;
   parser->type = t;
