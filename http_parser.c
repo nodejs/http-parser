03e0d5292aa7d9542a9c5306d6ea867e820dd255
James McLaughlin <jamie@lacewing-project.org>
2012-01-27 01:58:05 +0000

    Use "" instead of <> for the http_parser.h include.
    This avoids having to specify -I when building.

--- http_parser.c
+++ http_parser.c
@@ -21,7 +21,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-#include <http_parser.h>
+#include "http_parser.h"
 #include <assert.h>
 #include <stddef.h>
 #include <ctype.h>
