a828edaf6af085e8fe9ad4a38267a1e75eb3269a
Bertrand Paquet <bpaquet@octo.com>
2012-07-25 00:36:04 +0200

    Add a comment

--- http_parser.c
+++ http_parser.c
@@ -2121,6 +2121,7 @@ http_parser_parse_url(const char *buf, size_t buflen, int is_connect,
   }
 
   /* host must be present if there is a schema */
+  /* parsing http:///toto will fail */
   if ((u->field_set & ((1 << UF_SCHEMA) | (1 << UF_HOST))) != 0) {
     if (http_parse_host(buf, u, found_at) != 0) {
       return 1;
