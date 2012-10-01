48cbc6eb13c296bad11efe172228e77019349fc8
Juan José Pereira <juanper@uol.com.br>
2012-10-01 10:46:11 -0300

    Make compatible with VS 2005 32 bits compiler

--- http_parser.h
+++ http_parser.h
@@ -38,7 +38,9 @@ typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
+#if _MSC_VER<1400
 typedef SIZE_T size_t;
+#endif 
 typedef SSIZE_T ssize_t;
 #else
 #include <stdint.h>
