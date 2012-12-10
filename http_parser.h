2b77aba93a8e30a222f1fc29cbf93d1f5af166ea
James McLaughlin <jamie@lacewing-project.org>
2012-12-10 18:18:36 +0000

    Fix MSVS 2005 and 2008 missing size_t typdef.
    Replace manual size_t typedef with stddef.h include
    
    Fixes #128, #141.

--- http_parser.h
+++ http_parser.h
@@ -30,6 +30,7 @@ extern "C" {
 #include <sys/types.h>
 #if defined(_WIN32) && !defined(__MINGW32__) && (!defined(_MSC_VER) || _MSC_VER<1600)
 #include <BaseTsd.h>
+#include <stddef.h>
 typedef __int8 int8_t;
 typedef unsigned __int8 uint8_t;
 typedef __int16 int16_t;
@@ -38,8 +39,6 @@ typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
-typedef SIZE_T size_t;
-typedef SSIZE_T ssize_t;
 #else
 #include <stdint.h>
 #endif
