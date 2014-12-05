6461b2cd0bb1615f11dc33ef508c76594c7a5579
Fedor Indutny <fedor@indutny.com>
2014-12-05 18:10:59 +0300

    src: specialize tokens array to remove branch

--- http_parser.c
+++ http_parser.c
@@ -187,39 +187,45 @@ static const char *method_strings[] =
  *                    | "/" | "[" | "]" | "?" | "="
  *                    | "{" | "}" | SP | HT
  */
-static const char tokens[256] = {
-/*   0 nul    1 soh    2 stx    3 etx    4 eot    5 enq    6 ack    7 bel  */
-        0,       0,       0,       0,       0,       0,       0,       0,
-/*   8 bs     9 ht    10 nl    11 vt    12 np    13 cr    14 so    15 si   */
-        0,       0,       0,       0,       0,       0,       0,       0,
-/*  16 dle   17 dc1   18 dc2   19 dc3   20 dc4   21 nak   22 syn   23 etb */
-        0,       0,       0,       0,       0,       0,       0,       0,
-/*  24 can   25 em    26 sub   27 esc   28 fs    29 gs    30 rs    31 us  */
-        0,       0,       0,       0,       0,       0,       0,       0,
-/*  32 sp    33  !    34  "    35  #    36  $    37  %    38  &    39  '  */
-        0,      '!',      0,      '#',     '$',     '%',     '&',    '\'',
-/*  40  (    41  )    42  *    43  +    44  ,    45  -    46  .    47  /  */
-        0,       0,      '*',     '+',      0,      '-',     '.',      0,
-/*  48  0    49  1    50  2    51  3    52  4    53  5    54  6    55  7  */
-       '0',     '1',     '2',     '3',     '4',     '5',     '6',     '7',
-/*  56  8    57  9    58  :    59  ;    60  <    61  =    62  >    63  ?  */
-       '8',     '9',      0,       0,       0,       0,       0,       0,
-/*  64  @    65  A    66  B    67  C    68  D    69  E    70  F    71  G  */
-        0,      'a',     'b',     'c',     'd',     'e',     'f',     'g',
-/*  72  H    73  I    74  J    75  K    76  L    77  M    78  N    79  O  */
-       'h',     'i',     'j',     'k',     'l',     'm',     'n',     'o',
-/*  80  P    81  Q    82  R    83  S    84  T    85  U    86  V    87  W  */
-       'p',     'q',     'r',     's',     't',     'u',     'v',     'w',
-/*  88  X    89  Y    90  Z    91  [    92  \    93  ]    94  ^    95  _  */
-       'x',     'y',     'z',      0,       0,       0,      '^',     '_',
-/*  96  `    97  a    98  b    99  c   100  d   101  e   102  f   103  g  */
-       '`',     'a',     'b',     'c',     'd',     'e',     'f',     'g',
-/* 104  h   105  i   106  j   107  k   108  l   109  m   110  n   111  o  */
-       'h',     'i',     'j',     'k',     'l',     'm',     'n',     'o',
-/* 112  p   113  q   114  r   115  s   116  t   117  u   118  v   119  w  */
-       'p',     'q',     'r',     's',     't',     'u',     'v',     'w',
-/* 120  x   121  y   122  z   123  {   124  |   125  }   126  ~   127 del */
+#define TOKEN_TPL(NAME, SP)                                                   \
+static const char NAME[256] = {                                              \
+/*   0 nul    1 soh    2 stx    3 etx    4 eot    5 enq    6 ack    7 bel  */ \
+        0,       0,       0,       0,       0,       0,       0,       0,     \
+/*   8 bs     9 ht    10 nl    11 vt    12 np    13 cr    14 so    15 si   */ \
+        0,       0,       0,       0,       0,       0,       0,       0,     \
+/*  16 dle   17 dc1   18 dc2   19 dc3   20 dc4   21 nak   22 syn   23 etb */  \
+        0,       0,       0,       0,       0,       0,       0,       0,     \
+/*  24 can   25 em    26 sub   27 esc   28 fs    29 gs    30 rs    31 us  */  \
+        0,       0,       0,       0,       0,       0,       0,       0,     \
+/*  32 sp    33  !    34  "    35  #    36  $    37  %    38  &    39  '  */  \
+       SP,      '!',      0,      '#',     '$',     '%',     '&',    '\'',    \
+/*  40  (    41  )    42  *    43  +    44  ,    45  -    46  .    47  /  */  \
+        0,       0,      '*',     '+',      0,      '-',     '.',      0,     \
+/*  48  0    49  1    50  2    51  3    52  4    53  5    54  6    55  7  */  \
+       '0',     '1',     '2',     '3',     '4',     '5',     '6',     '7',    \
+/*  56  8    57  9    58  :    59  ;    60  <    61  =    62  >    63  ?  */  \
+       '8',     '9',      0,       0,       0,       0,       0,       0,     \
+/*  64  @    65  A    66  B    67  C    68  D    69  E    70  F    71  G  */  \
+        0,      'a',     'b',     'c',     'd',     'e',     'f',     'g',    \
+/*  72  H    73  I    74  J    75  K    76  L    77  M    78  N    79  O  */  \
+       'h',     'i',     'j',     'k',     'l',     'm',     'n',     'o',    \
+/*  80  P    81  Q    82  R    83  S    84  T    85  U    86  V    87  W  */  \
+       'p',     'q',     'r',     's',     't',     'u',     'v',     'w',    \
+/*  88  X    89  Y    90  Z    91  [    92  \    93  ]    94  ^    95  _  */  \
+       'x',     'y',     'z',      0,       0,       0,      '^',     '_',    \
+/*  96  `    97  a    98  b    99  c   100  d   101  e   102  f   103  g  */  \
+       '`',     'a',     'b',     'c',     'd',     'e',     'f',     'g',    \
+/* 104  h   105  i   106  j   107  k   108  l   109  m   110  n   111  o  */  \
+       'h',     'i',     'j',     'k',     'l',     'm',     'n',     'o',    \
+/* 112  p   113  q   114  r   115  s   116  t   117  u   118  v   119  w  */  \
+       'p',     'q',     'r',     's',     't',     'u',     'v',     'w',    \
+/* 120  x   121  y   122  z   123  {   124  |   125  }   126  ~   127 del */  \
        'x',     'y',     'z',      0,      '|',      0,      '~',       0 };
+
+TOKEN_TPL(strict_tokens, '\0');
+#if !HTTP_PARSER_STRICT
+TOKEN_TPL(tokens, ' ');
+#endif
 
 
 static const int8_t unhex[256] =
@@ -420,14 +426,14 @@ enum http_host_state
   (c) == ';' || (c) == ':' || (c) == '&' || (c) == '=' || (c) == '+' || \
   (c) == '$' || (c) == ',')
 
-#define STRICT_TOKEN(c)     (tokens[(unsigned char)c])
+#define STRICT_TOKEN(c)     (strict_tokens[(unsigned char)c])
 
 #if HTTP_PARSER_STRICT
-#define TOKEN(c)            (tokens[(unsigned char)c])
+#define TOKEN(c)            STRICT_TOKEN(c)
 #define IS_URL_CHAR(c)      (BIT_AT(normal_url_char, (unsigned char)c))
 #define IS_HOST_CHAR(c)     (IS_ALPHANUM(c) || (c) == '.' || (c) == '-')
 #else
-#define TOKEN(c)            ((c == ' ') ? ' ' : tokens[(unsigned char)c])
+#define TOKEN(c)            (tokens[(unsigned char)c])
 #define IS_URL_CHAR(c)                                                         \
   (BIT_AT(normal_url_char, (unsigned char)c) || ((c) & 0x80))
 #define IS_HOST_CHAR(c)                                                        \
