104bab75f2068211852825e93bbbc3b0ea98c903
Daniel Knoppel (Phusion) <daniel@phusion.nl>
2015-05-18 14:01:22 +0200

    Fix invalid memory access. http_parse_host() depends on u->field_data[UF_HOST], but this if() allowed the method to be called if only u->field_data[UF_SCHEMA] was set, resulting in use of unintialized pointers.

--- http_parser.c
+++ http_parser.c
@@ -2376,7 +2376,7 @@ http_parser_parse_url(const char *buf, size_t buflen, int is_connect,
 
   /* host must be present if there is a schema */
   /* parsing http:///toto will fail */
-  if ((u->field_set & ((1 << UF_SCHEMA) | (1 << UF_HOST))) != 0) {
+  if ((u->field_set & (1 << UF_SCHEMA)) != 0 && (u->field_set & (1 << UF_HOST)) != 0) {
     if (http_parse_host(buf, u, found_at) != 0) {
       return 1;
     }
