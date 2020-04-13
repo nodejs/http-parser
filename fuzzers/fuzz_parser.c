6df1ebfb7d61431e9b9910309585bc27e37e7b6f
davkor <david@adalogics.com>
2020-04-13 17:56:55 +0100

    Fixed indentation in struct.

--- fuzzers/fuzz_parser.c
+++ fuzzers/fuzz_parser.c
@@ -7,15 +7,15 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
   static http_parser_settings settings_null = {
     .on_message_begin = 0
-	, .on_header_field = 0
-	,.on_header_value = 0
-	,.on_url = 0
-	,.on_status = 0
-	,.on_body = 0
-	,.on_headers_complete = 0
-	,.on_message_complete = 0
-	,.on_chunk_header = 0
-	,.on_chunk_complete = 0
+    , .on_header_field = 0
+    ,.on_header_value = 0
+    ,.on_url = 0
+    ,.on_status = 0
+    ,.on_body = 0
+    ,.on_headers_complete = 0
+    ,.on_message_complete = 0
+    ,.on_chunk_header = 0
+    ,.on_chunk_complete = 0
   };
 
   http_parser parser;
